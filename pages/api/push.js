3b3453b41dde06c30b8b8f2974092e0fb6efb5d6
M1chae1M <michalmaru10@gmail.com>
2023-05-09 01:34:06 +0200

    new url

--- pages/api/push.js
+++ pages/api/push.js
@@ -3,12 +3,12 @@ import {myPromise} from './promiseF';
 export const JWT = require('jsonwebtoken');
 
 module.exports=(req, res)=>{
-    const {body}=req
-    const {token, newAlerts}=body
-    const stringed=JSON.stringify(newAlerts)
-    const decodedData=JWT.decode(token,verifiKey)
-    // const query=`UPDATE ${process.env.DB_ALERTS_TABLE} SET alerts = '${stringed}' WHERE ${process.env.DB_ALERTS_TABLE}.user = '${decodedData.login}';`
-    const query=`UPDATE alerts SET alerts = '${stringed}' WHERE alerts.user = '${decodedData.login}';`
+    const {body}=req;
+    const {token, newAlerts}=body??'';
+    const stringed=JSON.stringify(newAlerts);
+    const decodedData=JWT.decode(token,verifiKey);
+    // const query=`UPDATE ${process.env.DB_ALERTS_TABLE} SET alerts = '${stringed}' WHERE ${process.env.DB_ALERTS_TABLE}.user = '${decodedData.login}';`;
+    const query=`UPDATE alerts SET alerts = '${stringed}' WHERE alerts.user = '${decodedData.login}';`;
 
-    myPromise(query)
+    myPromise(query);
 }
