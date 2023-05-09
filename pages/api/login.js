bc15c8dbb556e666de92f673ca0fbb64dc2135b4
M1chae1M <michalmaru10@gmail.com>
2023-05-09 22:53:24 +0200

    new

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,3 +1,29 @@
+// import {verifiKey} from './verifiKey';
+// import {myPromise} from './promiseF';
+// import {ConnectionWithDB} from "./connectionWithDB";
+
+// export const mysql=require('mysql2');
+// export const jwt=require('jsonwebtoken');
+
+// module.exports=(req, res)=>{
+//     const {body}=req;
+//     const {login, password}=body;
+//     const query=`SELECT profile.login from profile WHERE profile.login="${login}" AND profile.password=SHA2('${password}', 256)`;
+//     const queryForAlerts=`SELECT * from alerts WHERE alerts.user="${login}"`;
+//     const verifiToken=jwt.sign({login, password}, verifiKey);
+
+//     myPromise(query,(err)=>{console.log(err)})
+//     .then((data)=>{
+//         if(data.length>0){
+//             myPromise(queryForAlerts,(err)=>{console.log(err)})
+//             .then(alrt=>res.status(200).json({token:verifiToken, logged:true, message:'', alerts:alrt[0]}))
+//         }
+//         else{
+//             res.status(200).json({message:'Login failed!!!', logged:false})
+//         }
+//     })
+// }
+
 import {verifiKey} from './verifiKey';
 import {myPromise} from './promiseF';
 import {ConnectionWithDB} from "./connectionWithDB";
@@ -8,14 +34,27 @@ export const jwt=require('jsonwebtoken');
 module.exports=(req, res)=>{
     const {body}=req;
     const {login, password}=body;
-    const query=`SELECT profile.login from profile WHERE profile.login="${login}" AND profile.password=SHA2('${password}', 256)`;
+    const query=`SELECT profile.login from profile WHERE profile.login="${login}" AND ${process.env.DB_PROFILE_TABLE}.password=SHA2('${password}', 256)`;
+    // const queryForAlerts=`SELECT * from ${process.env.DB_ALERTS_TABLE} WHERE ${process.env.DB_ALERTS_TABLE}.user="${login}"`;
     const queryForAlerts=`SELECT * from alerts WHERE alerts.user="${login}"`;
     const verifiToken=jwt.sign({login, password}, verifiKey);
 
-    myPromise(query,(err)=>{console.log(err)})
+    myPromise(query)
+    // const connection=ConnectionWithDB();
+    // new Promise((resolve, reject) => {
+    //     connection.query(query, (error, results, fields) => {
+    //     error && reject(error);
+    //     resolve(results);
+    //     });
+    // })
     .then((data)=>{
+        // console.log(data)
+        // console.log(data.length)
+        // console.log(data.length>0)
+
+        // data.length>0 ? console.log('tak'):console.log('nie')
         if(data.length>0){
-            myPromise(queryForAlerts,(err)=>{console.log(err)})
+            myPromise(queryForAlerts)
             .then(alrt=>res.status(200).json({token:verifiToken, logged:true, message:'', alerts:alrt[0]}))
         }
         else{
