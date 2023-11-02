4309f50be98995146f74c05ded657ff918ffc4eb
M1chae1M <michalmaru10@gmail.com>
2023-11-02 11:46:48 +0100

    test

--- pages/api/singup/index.js
+++ pages/api/singup/index.js
@@ -4,10 +4,7 @@ import {myPromise} from '../promiseF';
 export default async(req, res)=>{
     const {body}=req;
     const {login, password}=body;
-    // const query=`SELECT * FROM profile`;
-
     const data=await DB_instance.select('profile','*',{})
-
 
     if(login===''||password===''||password===login){
         res.status(409).json({message:'Illegal login or password!'})
@@ -16,31 +13,10 @@ export default async(req, res)=>{
         if(data.filter(x=>x.login===login).length>0) res.status(409).json({message:'login busy'})
         else{
             const addNewUserQuery=`INSERT INTO profile (login, password) VALUES ('${login}', SHA2('${password}', 256));`;
-            // const addNewAlertsQuery=`INSERT INTO alerts (user, alerts) VALUES ('${login}', '');`;
 
             myPromise(addNewUserQuery)
-            // myPromise(addNewAlertsQuery)
             await DB_instance.insert('alerts',{user:login,alerts:''})
             res.status(200).json({message:'created new user'})
         }
     }
-
-
-    // myPromise(query)
-    // .then(data=>{
-    //     if(login===''||password===''||password===login){
-    //         res.status(409).json({message:'Illegal login or password!'})
-    //     }
-    //     else{
-    //         if(data.filter(x=>x.login===login).length>0) res.status(409).json({message:'login busy'})
-    //         else{
-    //             const addNewUserQuery=`INSERT INTO profile (login, password) VALUES ('${login}', SHA2('${password}', 256));`;
-    //             const addNewAlertsQuery=`INSERT INTO alerts (user, alerts) VALUES ('${login}', '');`;
-
-    //             myPromise(addNewUserQuery)
-    //             myPromise(addNewAlertsQuery)
-    //             res.status(200).json({message:'created new user'})
-    //         }
-    //     }
-    // })
 }
